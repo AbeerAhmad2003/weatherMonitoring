3beebeef9115d40dc9e27b62d70c2ceffce5f148
AbeerAhmad2003 <abeerahmad159357@gmail.com>
2025-08-25 15:58:25 +0300

    add parserFactory

---
+++ ParserFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weatherProj1
+{
+    public static class ParserFactory
+    {
+        public static IWeatherDataParser Create(string type)
+        {
+            return type.ToLower() switch
+            {
+                "json" => new JsonWeatherDataParser(),
+                "xml" => new XmlWeatherParser(),
+                _ => throw new ArgumentException("Unsupported parser type.")
+            };
+        }
+    }
+}
