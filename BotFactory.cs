0b2ed2e5a6a34b374af257c040c5b34fc3ad2f0b
AbeerAhmad2003 <abeerahmad159357@gmail.com>
2025-08-25 16:12:02 +0300

    add bot factory

---
+++ BotFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace weatherProj1
+{
+    public static class BotFactory
+    {
+        public static List<WeatherBotBase> CreateBots(string configJson)
+        {
+            var bots = new List<WeatherBotBase>();
+            var config = JsonSerializer.Deserialize<Dictionary<string, BotConfig>>(configJson);
+            if (config == null) return bots;
+
+            foreach (var entry in config)
+            {
+                switch (entry.Key)
+                {
+                    case "RainBot":
+                        if (entry.Value.Enabled)
+                            bots.Add(new RainBot(entry.Value));
+                        break;
+
+                    case "SunBot":
+                        if (entry.Value.Enabled)
+                            bots.Add(new SunBot(entry.Value));
+                        break;
+
+                    case "SnowBot":
+                        if (entry.Value.Enabled)
+                            bots.Add(new SnowBot(entry.Value));
+                        break;
+                }
+            }
+
+            return bots;
+        }
+
+    }
+}
