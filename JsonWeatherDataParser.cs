9638ec0fe70387eff8ad72cb10320b2365e245f4
AbeerAhmad2003 <abeerahmad159357@gmail.com>
2025-08-25 15:55:46 +0300

    add weatherdata parsers

---
+++ JsonWeatherDataParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace weatherProj1
+{
+    public class JsonWeatherDataParser : IWeatherDataParser
+    {
+        public WeatherData? Parse(string input)
+        {
+            try
+            {
+                var data = JsonSerializer.Deserialize<WeatherData>(input);
+                if (data == null)
+                {
+                    Console.WriteLine("JSON input is empty or invalid.");
+
+                }
+                return data;
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Invalid JSON format: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
